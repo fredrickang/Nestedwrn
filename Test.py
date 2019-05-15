5f1b782941e8659cda9b4382b2fc032be80bfaa8
Fredrickang <fredrickang@Fredrickangui-MacBookPro.local>
2019-05-15 17:47:59 +0900

    changed

--- Test.py
+++ Test.py
@@ -5,7 +5,7 @@ import pickle as pk
 # all task level 3 
 #       batch size = 125
 testbed_1 = Train()
-time_log , err_log =testbed_1.test(3,125,FLAGS.ckpt_path)
+time_log , err_log =testbed_1.test(2,125,FLAGS.ckpt_path)
 logs = [time_log, err_log]
 fo = open('3-125.pk','wb')
 pk.dump(logs,fo)
@@ -13,7 +13,7 @@ fo.close()
 
 #       batch size = 1
 testbed_2 = Train()
-time_log, err_log = testbed_2.test(3,1,FLAGS.ckpt_path)
+time_log, err_log = testbed_2.test(2,1,FLAGS.ckpt_path)
 logs = [time_log, err_log]
 fo = open('3-1.pk','wb')
 pk.dump(logs,fo)
@@ -22,7 +22,7 @@ fo.close()
 # all task level 1
 #       batch size = 125
 testbed_3 = Train()
-time_log, err_log = testbed_3.test(1,125,FLAGS.ckpt_path)
+time_log, err_log = testbed_3.test(0,125,FLAGS.ckpt_path)
 logs = [time_log, err_log]
 fo = open('1-125.pk','wb')
 pk.dump(logs,fo)
@@ -30,7 +30,7 @@ fo.close()
 
 #       batch size = 1
 testbed_4 = Train()
-time_log, err_log = testbed_4.test(1,1,FLAGS.ckpt_path)
+time_log, err_log = testbed_4.test(0,1,FLAGS.ckpt_path)
 logs = [time_log, err_log]
 fo = open('1-1.pk','wb')
 pk.dump(logs,fo)
